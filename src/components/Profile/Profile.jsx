af4b88952b1ec1614419353b6529e37383e2cf7e
JuliaPriadka <150530521+JuliaPriadka@users.noreply.github.com>
2024-05-12 19:20:57 +0300

    Update Profile jsx & css

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -13,15 +13,15 @@ export default function Profile({ name, tag, location, image, stats }) {
       <ul className={css.profileStats}>
         <li className={css.profileStatsItem}>
           <span>Followers</span>
-          <span>{stats.followers}</span>
+          <span className={css.statsNumber}>{stats.followers}</span>
         </li>
-        <li>
+        <li className={css.profileStatsItem}>
           <span>Views</span>
-          <span>{stats.views}</span>
+          <span className={css.statsNumber}>{stats.views}</span>
         </li>
-        <li>
+        <li className={css.profileStatsItem}>
           <span>Likes</span>
-          <span>{stats.likes}</span>
+          <span className={css.statsNumber}>{stats.likes}</span>
         </li>
       </ul>
     </div>
