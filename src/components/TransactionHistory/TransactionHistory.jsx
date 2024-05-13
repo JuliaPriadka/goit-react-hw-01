19d0d5641835f1a13fdea4d89d68f152feb555b2
JuliaPriadka <150530521+JuliaPriadka@users.noreply.github.com>
2024-05-13 18:49:19 +0300

    Update TransactionHistory jsx & css

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -1,26 +1,26 @@
-import css from "../TransactionHistory/TransactionHistory.module.css"
+import css from '../TransactionHistory/TransactionHistory.module.css';
 
 export default function TransactionHistory({ items }) {
   return (
     <table className={css.table}>
-      <thead >
+      <thead>
         <tr>
           <th>Type</th>
           <th>Amount</th>
           <th>Currency</th>
         </tr>
       </thead>
-      {items.map(item => {
-        return (
-          <tbody key={item.key} className={css.row}>
-            <tr className={css.tableRow}>
+      <tbody className={css.row}>
+        {items.map(item => {
+          return (
+            <tr className={css.tableRow} key={item.key}>
               <td className={css.type}>{item.type}</td>
               <td className={css.amount}>{item.amount}</td>
               <td className={css.currency}>{item.currency}</td>
             </tr>
-          </tbody>
-        );
-      })}
+          );
+        })}
+      </tbody>
     </table>
   );
 }
